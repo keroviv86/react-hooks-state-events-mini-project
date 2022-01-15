540579b0ce00ccc3558e342d3d0284890742cf25
Vivian Yim <keroviv86@gmail.com>
2022-01-15 16:31:32 -0600

    commit tree f48e52c2cf01de52492f6473398f8b0827eb9dc3

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, handleFilter, selectedCategory}) {
+  let categoryButtons = categories.map(category => (
+    <button value ={category} key = {category} onClick={handleFilter} className = {category === selectedCategory ? "selected" : null} >{category}</button>
+  ))
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categoryButtons}
     </div>
   );
 }
